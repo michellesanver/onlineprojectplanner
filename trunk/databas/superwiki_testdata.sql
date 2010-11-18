fb79bdc89e6f93221a90a489098ff6029d94337f
tazzie76 <tazzie76@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-11-18 15:53:56 +0000

    uppdaterade med en ny kolumn i user

---
+++ trunk/databas/superwiki_testdata.sql
@@ -0,0 +1,6 @@
+
+--
+-- Dumping data for table `User`
+--
+
+INSERT INTO `User` VALUES(4, 'Ronaaald', 'Mcccdonald', 'ronald@gmail.com', 'bf199c2cd79a9d9080baed710e4763', 'ronald', 'brandonstreet', 90210, 'new york');
