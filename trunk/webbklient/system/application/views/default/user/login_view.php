84b1d86468a4a923b71be8e913d3abc49ce6fcc4
michelle.sanver <michelle.sanver@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-11-23 11:55:28 +0000

    Har börjat lite smått på themes och lagt till loginformulär :)

--- trunk/webbklient/system/application/views/default/user/login_view.php
+++ trunk/webbklient/system/application/views/default/user/login_view.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<h1>Login</h1>
+<?php
+    if(isset($status)) {
+        echo "<div class='" . $status . "'><b>" . $status_message . "</b></div>";
+    }
+?>
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv">
+<?php if (isset($hideForm) == false || $hideForm == false) { ?>
+    <form action="<?php echo site_url('user_controller/login'); ?>" method="POST">
 
-	<head>
-    	<title>non</title>
-   		<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />  
-	</head>
-	<body>
-			<H1>superwiki</H1>
-<h5>Logga in</h5>
-
-
-</body>
-</html>
+        <p><label for="username">Username</label>:<input id="username" type="text" name="username" value="<?php echo (isset($username)) ? $username : ""; ?>" /></p>
+        
+        <p><label for="password">Password</label>:<input id="password" type="text" name="password" value="<?php echo (isset($password)) ? $password : ""; ?>" /></p>
+        
+        <p><input type="submit" value="Login" name="submit" /></p>
+    </form>
+    
+    <p>Don't have an account? <a href="<?php echo site_url('user_controller/register'); ?>">Register here</a></p>
+<?php } ?>
