e0f3074b2de5a6b8d01ecd14b9fa7e0d3e2d303d
limpan071 <limpan071@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-11-23 21:48:58 +0000

    fix

--- webbklient/system/application/views/default/user/login_view.php
+++ webbklient/system/application/views/default/user/login_view.php
@@ -1,14 +1,4 @@
-<<<<<<< .mine
-=======
-<h1>Login</h1>
-<?php
-    if(isset($status)) {
-        echo "<div class='" . $status . "'><b>" . $status_message . "</b></div>";
-    }
-?>
->>>>>>> .r105
 
-<<<<<<< .mine
 <div id="login_box">
 	<?php
 		if(isset($status)) {
@@ -22,12 +12,6 @@
 		<p><input type="submit" value="Sign in" name="login_btn" /></p>
 	</form>
 </div>
-=======
-<?php if (isset($hideForm) == false || $hideForm == false) { ?>
-    <form action="<?php echo site_url('user_controller/login'); ?>" method="POST">
->>>>>>> .r105
-
-<<<<<<< .mine
 
 
 
@@ -36,14 +20,4 @@
 
 
 
-=======
-        <p><label for="username">Username</label>:<input id="username" type="text" name="username" value="<?php echo (isset($username)) ? $username : ""; ?>" /></p>
-        
-        <p><label for="password">Password</label>:<input id="password" type="text" name="password" value="<?php echo (isset($password)) ? $password : ""; ?>" /></p>
-        
-        <p><input type="submit" value="Login" name="submit" /></p>
-    </form>
-    
-    <p>Don't have an account? <a href="<?php echo site_url('user_controller/register'); ?>">Register here</a></p>
-<?php } ?>
->>>>>>> .r105
+
