a17db0c4f0c86b78a61400fcb194b5276b5b67f9
limpan071 <limpan071@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-11-23 21:42:27 +0000

    fix

--- webbklient/system/application/controllers/main_controller.php
+++ webbklient/system/application/controllers/main_controller.php
@@ -2,9 +2,22 @@
 
 class Main_controller extends Controller 
 {
+	
+	function Main_controller()
+	{
+		parent::Controller();	
+		
+		$this->load->library('validation');
+	}
+	
 	function index()
 	{
 		$this->theme->view('user/login_view');		
 	}
-
+	function register()
+	{
+		$this->theme->view('user/register');	
+	}
+	
+	
 }
