7aca749ec886b9da360445a2aa039571fe6d415e
michelle.sanver <michelle.sanver@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-11-30 12:35:32 +0000

    Lite mer fix!

--- trunk/webbklient/system/application/views/default/project/index.php
+++ trunk/webbklient/system/application/views/default/project/index.php
@@ -1,5 +1,12 @@
 <h1>Project listing</h1>
 <p>Lists all of your projects</p>
-<?php
-	var_dump($projects);
-?>
+	<div class="newprojectbox">
+		<h2>New project</h2>
+		<p><a href="<?php echo site_url('project_controller/register'); ?>">Create a new project</a></p>
+	</div>
+<?php foreach($projects as $project): ?>
+	<div class="projectbox">
+		<h2><?php echo($project['Title']);?></h2>
+		<p><?php echo($project['Description']);?></p>
+	</div>
+<?php endforeach; ?>
