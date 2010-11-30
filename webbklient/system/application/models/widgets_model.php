1901ef5b4f0a1e5a1442bfe8bfa18aa2ba037ca3
tazzie76 <tazzie76@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-11-30 12:30:29 +0000

    user has en funktion för att retunera userid om inloggad

---
+++ webbklient/system/application/models/widgets_model.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* This class has all database-logic for
+* the class Widgets
+*
+* @link https://code.google.com/p/onlineprojectplanner/
+*/
+
+class Widgets_model extends Model  {
+    
+     private $_table = "Project_widgets";
+    
+     function GetProjectWidgets($userID, $projectID)
+     {
+         
+         
+         
+         
+         
+         
+     }
+    
+    
+}
