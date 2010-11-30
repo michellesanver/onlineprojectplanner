56f0ba205a8c9e3812891a561de66d03e206ce8f
michelle.sanver <michelle.sanver@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-11-30 14:31:42 +0000

    Lite uppdatering till A-taggar

--- trunk/webbklient/system/application/views/default/project/index.php
+++ trunk/webbklient/system/application/views/default/project/index.php
@@ -1,13 +1,12 @@
 <h1 class="blackheader">Projects</h1>
 <div id="projectlisting">
-	<div class="newprojectbox">
-		<h2 class="project_title">New project</h2>
-		<p class="project_description"><a href="<?php echo site_url('project_controller/register'); ?>">Create a new project</a></p>
-	</div>
+	<a class="newprojectbox" href="<?php echo site_url('project_controller/register'); ?>">
+		<b class="new_project_title">New project</b>
+	</a>
 <?php foreach($projects as $project): ?>
-	<div class="projectbox">
-		<h2 class="project_title"><?php echo($project['Title']);?></h2>
-		<p class="project_description"><?php echo($project['Description']);?></p>
-	</div>
+	<a class="projectbox">
+		<b class="project_title"><?php echo($project['Title']);?></b><br/>
+		<?php echo($project['Description']);?>
+	</a>
 <?php endforeach; ?>
 </div>
