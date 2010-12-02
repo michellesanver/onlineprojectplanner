10b1ec5aafa239fc1c2b57abe03e65cdf1574f47
michelle.sanver <michelle.sanver@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-02 11:16:38 +0000

    Maybe something like this?

--- trunk/webbklient/system/application/views/default/user/login_view.php
+++ trunk/webbklient/system/application/views/default/user/login_view.php
@@ -1,16 +1,25 @@
-<div id="loginbackground">
-
 	<?php
 		if(isset($status)) {
 			echo "<div class='" . $status . "'><b>" . $status_message . "</b>" . $this->validation->error_string . "<p>" . validation_errors() . "</p></div>";
 		}
 	?>
-	<h1>Login</h1>
-	<form id="loginform" action="<?php echo site_url('account/login'); ?>" method="POST">
+	<h1 class="blackheader">Authenticate</h1>
+	
+	<form id="leftbox" action="<?php echo site_url('account/login'); ?>" method="POST">
 		<label for="username">Username: </label> <input type="text" name="username" value="<?php echo (isset($username)) ? $username : ""; ?>" />*<br/>
 		<label for="password">Password: </label> <input type="password" name="password" />*<br/>
-		<label>&nbsp; </label> <input type="submit" class="button" value="Sign in" name="login_btn" /><br/>
-		<p class="small_links"><a href="<?php echo site_url('account/register'); ?>">Click here to register</a> | <a href="<?php echo site_url('account/resetpassword'); ?>">Forgot password?</a></p>
+		
+		<label>&nbsp; </label><input type="submit" class="button" value="Sign in" name="login_btn" /><br/>
+		<p class="forgotpassword"><a href="<?php echo site_url('account/resetpassword'); ?>">Forgot password?</a></p>
 	</form>
-
-</div>
+	
+	<div id="rightbox">
+		<h2>Don't have an account? Register!</h2>
+		<ul>
+			<li>You can create your own projects.</li>
+			<li>You can invite other members.</li>
+			<li>Collaborate like never before</li>
+			<li>It's free!</li>
+		</ul>
+		<p><a class="register" href="<?php echo site_url('account/register'); ?>">Click here to register</a></p>
+	</div>
