757478463d886113875d55f797f97658e0deaf42
michelle.sanver <michelle.sanver@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-02 10:36:01 +0000

    Errostyling and p fix.

--- trunk/webbklient/system/application/views/default/user/login_view.php
+++ trunk/webbklient/system/application/views/default/user/login_view.php
@@ -8,8 +8,8 @@
 	<h1>Login</h1>
 	<form id="loginform" action="<?php echo site_url('account/login'); ?>" method="POST">
 		<label for="username">Username: </label> <input type="text" name="username" value="<?php echo (isset($username)) ? $username : ""; ?>" />*<br/>
-		<label for="password">Password: </label> <input type="password" name="password" />*</p>
-		<label>&nbsp; </label> <input type="submit" class="button" value="Sign in" name="login_btn" /></p>
+		<label for="password">Password: </label> <input type="password" name="password" />*<br/>
+		<label>&nbsp; </label> <input type="submit" class="button" value="Sign in" name="login_btn" /><br/>
 		<p class="small_links"><label>&nbsp; </label><a href="<?php echo site_url('account/register'); ?>">Click here to register</a> | <a href="<?php echo site_url('account/resetpassword'); ?>">Forgot password?</a></p>
 	</form>
 
