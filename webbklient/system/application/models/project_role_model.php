b60f814773216aa476f4b00d2f29c7c9db6790ff
astrom.tobias <astrom.tobias@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-02 21:08:36 +0000

    Added model for Project_Role

---
+++ webbklient/system/application/models/project_role_model.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* This class has all database-logic for
+* the table Project_Role
+*
+* @link https://code.google.com/p/onlineprojectplanner/
+*/
+
+class Project_role_model extends Model
+{
+    private $_table = "Project_Role";
+    
+    /**
+    * Function: getByRole
+    * This function will return an array representing
+    * the role information of the selected role.
+    *
+    * @param int $role
+    * @return mixed
+    */
+    function getByRole($role)
+    {
+
+        $this->db->where('Role', $role);
+        $this->db->limit(1);
+        $query = $this->db->get($this->_table);
+        
+        if($query->num_rows() > 0) {
+
+            return $query->row(0);
+        }
+        else {
+
+            // Role not found
+
+            return false;
+        }
+
+    }
+
+}
