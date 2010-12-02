eac7e472e80fa5a122c2560e3135f53289995880
michelle.sanver <michelle.sanver@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-02 10:24:32 +0000

    Forgot the links.

--- webbklient/system/application/views/default/user/login_view.php
+++ webbklient/system/application/views/default/user/login_view.php
@@ -10,8 +10,7 @@
 		<label for="username">Username: </label> <input type="text" name="username" value="<?php echo (isset($username)) ? $username : ""; ?>" />*<br/>
 		<label for="password">Password: </label> <input type="password" name="password" />*</p>
 		<label>&nbsp; </label> <input type="submit" class="button" value="Sign in" name="login_btn" /></p>
-		<p><a href="<?php echo site_url('account/register'); ?>">Click here to register</a></p>
-		<p><a href="<?php echo site_url('account/resetpassword'); ?>">Forgot password?</a></p>
+		<p class="small_links"><label>&nbsp; </label><a href="<?php echo site_url('account/register'); ?>">Click here to register</a> | <a href="<?php echo site_url('account/resetpassword'); ?>">Forgot password?</a></p>
 	</form>
 
 </div>
