6ff0b0f31edbedcce28f9ec330c7f81536b65020
astrom.tobias <astrom.tobias@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-04 09:46:33 +0000

    Added some functionality for members/invite (Not yet fully implemented)

--- trunk/webbklient/system/application/models/project_member_model.php
+++ trunk/webbklient/system/application/models/project_member_model.php
@@ -12,6 +12,7 @@ class Project_member_model extends Model
 	
     private $_table = "Project_Member";
     private $_table_role = "Project_Role";
+    private $_table_user = "User";
     
     /**
     * Function: getByUserId
@@ -33,6 +34,34 @@ class Project_member_model extends Model
         $this->db->join($table2, "$table1.Project_role_id = $table2.Project_role_id");
 		$query = $this->db->get();
         
+		if($query && $query->num_rows() > 0 )
+			return $query->result_array();
+		else
+			return null;
+	}
+
+        /**
+    * Function: getByProjectId
+    * This function will return an array representing
+    * the memberships of the $projectID and also join with
+    * project_role and user.
+    *
+    * @param int $projectID
+    * @return mixed
+    */
+	function getByProjectId($projectID)
+	{
+        $table1 = $this->_table;
+        $table2 = $this->_table_user;
+        $table3 = $this->_table_role;
+
+        $this->db->select("$table1.*, $table2.*, $table3.Role");
+        $this->db->where(array('Project_id' => $projectID));
+        $this->db->from($table1);
+        $this->db->join($table2, "$table1.User_id = $table2.User_id");
+        $this->db->join($table3, "$table1.Project_role_id = $table3.Project_role_id");
+		$query = $this->db->get();
+
 		if($query && $query->num_rows() > 0 )
 			return $query->result_array();
 		else
