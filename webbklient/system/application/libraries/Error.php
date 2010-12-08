cf6f9f222754a88e9640e89fbffa89f942abff45
dennis.sangmo <dennis.sangmo@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-08 07:49:17 +0000

    Implemented error logging

---
+++ webbklient/system/application/libraries/Error.php
@@ -0,0 +1,73 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+* This class handles all functions
+* about error.
+*
+* @link https://code.google.com/p/onlineprojectplanner/
+*/
+class Error
+{ 
+	private $_CI = null;
+  private $_last_error = "";
+    
+	function __construct()
+	{
+		// get CI instance
+		$this->_CI = & get_instance();
+		
+		// load model for library
+		$this->_CI->load->model(array('Error_model'));
+	}
+	
+   /**
+    * This function will return the last error
+    * this class has set.
+    */
+    function GetLastError()
+    {
+        // save error, clear message and return
+        $returnStr = $this->_last_error;    
+        $this->_last_error = "";
+        return $returnStr;
+    }
+    
+   /**
+	* Return the full log from the database
+	* 
+	* @return array
+	*/
+	function GetFullLog()
+	{
+		return $this->_CI->Error_model->getAll();
+	}
+		
+    /**
+    * Will log an errormessage and inserts it to the database
+    * 
+    * @param string $message
+    * @param string $ip
+    * @return mixed
+    */
+    function Log($message, $ip)
+    {
+			$insert = array(
+				'Ip_adress' => $ip,
+				'Message' => $message
+			);
+			
+			$error_id = $this->_CI->Error_model->insert($insert);
+			
+			if($error_id > 0) {
+				return $error_id;
+			} else {
+				return false;
+			}
+    }
+		
+		function RemoveLogEntity($err_id) {
+			return $this->_CI->Error_model->delete($err_id);
+		}
+}
+
+?>
