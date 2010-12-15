fe35eaed6746a9c00ac2ac0883451a4555b455a6
tazzie76 <tazzie76@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-15 09:53:32 +0000

    flyttade om ifrån common.js till desktop.js och widget.js

--- trunk/webbklient/js/widget/widget.js
+++ trunk/webbklient/js/widget/widget.js
@@ -18,3 +18,102 @@ Widget.prototype.setContent = function(data) {
 Widget.prototype.getWindowObject = function() {
 	return this.wnd;
 };
+
+// display a ajax spinner
+Widget.prototype.show_ajax_loader = function(divID, divClass)
+{   
+     // class frame_loading is from jquery.window 
+     var container = null;
+     if (divClass != undefined || divClass != "" || divClass != null)
+     {
+        container = $('.'+divClass);
+     }
+     else
+     {
+         container = $('#'+divID);
+     }
+     
+     // show white or black version?  
+     if ( container.html() == "" )
+     {
+         // no content; show white
+         var loadingHTML = "<div class='frame_loading'>Loading...</div>"; 
+         container.html(loadingHTML);
+         var loading = container.children(".frame_loading");
+         loading.css("marginLeft",    '-' + (loading.outerWidth() / 2) -20 + 'px');
+     }
+     else
+     {
+        // has content; show black 
+         
+        // find top-most div of window
+        var parentContainer = container;
+        while (parentContainer.hasClass('window_panel')==false)
+        {
+            parentContainer = parentContainer.parent();    
+        } 
+        
+        // prepare html 
+        var loadingHTML = "<div class='frame_loading-black'>Loading...</div>"; 
+        
+        // prepare options for overlay
+        var overlayOptions = {  
+                                'z-index': 2000,
+                                'background-color': '#333',
+                                'height':parentContainer.css('height'),
+                                'width':parentContainer.css('width'),
+                                'opacity':'0.5',
+                                '-ms-filter':'"progid:DXImageTransform.Microsoft.Alpha(Opacity=50)"',
+                                'filter':'alpha(opacity=50)',
+                                'position': 'absolute',
+                                'top': 0,
+                                'left': 0
+                            }; 
+         
+        // append overlay to ID or Class     
+        var overlay = "";
+        if (divClass != undefined || divClass != "" || divClass != null)
+        {
+            overlay = "<div class=\""+divClass+"_Overlay\"></div>";
+            container.append(overlay);
+            $('.'+divClass+'_Overlay').css(overlayOptions);
+        }
+        else
+        {
+            overlay = "<div id=\""+divID+"_Overlay\"></div>";
+            container.append(overlay);
+            $('#'+divID+'_Overlay').css(overlayOptions);
+        }
+        
+        // append loading html and set position
+        container.append(loadingHTML);
+        var loading = container.children(".frame_loading-black");
+        loading.css({"marginLeft": '-' + (loading.outerWidth() / 2) -20 + 'px', 'z-index': 2001});  
+     }
+}
+
+// display an error (jquery ui)
+Widget.prototype.show_ajax_error = function(divID, divClass, loadURL, errorIcon)
+{
+    // prepare message
+    var errorMessage = "<p class=\"ajaxTemplateWidget_Error\">";
+    
+    // with icon?
+    if (errorIcon != undefined || errorIcon != "" || errorIcon != null)
+    {
+        errorMessage += "<img src=\""+errorIcon+"\" width=\"35\" height=\"35\" />";
+    }
+    
+    // append message
+    errorMessage += "Error: Unable to load the page at<br/><br/><small>"+loadURL+"</small></p>";
+
+    // show in div with ID or Class
+    if (divClass != undefined || divClass != "" || divClass != null)
+    {
+        $('.'+divClass).html(errorMessage);
+    }
+    else
+    {
+        $('#'+divID).html(errorMessage);
+    }
+}
