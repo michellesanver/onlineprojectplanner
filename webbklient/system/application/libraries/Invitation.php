93d2a4dc4a9af0dcf8f305ef221fa85139780870
astrom.tobias <astrom.tobias@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2010-12-18 15:27:40 +0000

    A project member can now only invite a new member to project members own role and/or roles lower in roles hierarchy

--- webbklient/system/application/libraries/Invitation.php
+++ webbklient/system/application/libraries/Invitation.php
@@ -38,6 +38,111 @@ class Invitation
     }
 
     /**
+    * Function: GetSuitableRolesForInvitation
+    * This function will return the suitable roles
+    * for certain member role to invite
+    *
+    * @param string $value
+    * @return bool
+    */
+
+    function GetSuitableRolesForInvitation()
+    {
+        // Fetches all available roles
+
+        $appRoles = $this->_CI->Project_role_model->getAll();
+
+        if($appRoles != null)
+        {
+            // Fetch userID
+
+            $userID = $this->_CI->session->userdata('UserID');
+
+            // Fetch memerships
+
+            $memberships = $this->_CI->Project_member_model->getByUserId($userID);
+
+            // Fetch currentProjectID
+
+            $currentProjectID = $this->_CI->session->userdata('current_project_id');
+
+            $roleInProject = null;
+
+            if($memberships != null)
+            {
+                foreach($memberships as $membership) {
+
+                    if($membership['User_id'] == $userID && $membership['Project_id'] == $currentProjectID)
+                    {
+                        $roleInProject = $membership['Role'];
+                    }
+
+                }
+            }
+
+            // Set up role structure to see what roles is equal or lower in hierarchy
+
+            $roleStructure = array();
+
+            $currentOrder = null;
+
+            // Set up role structure with needed information
+
+            while(count($roleStructure) < count($appRoles)) {
+
+                foreach($appRoles as $appRole) {
+
+                    if($currentOrder == null && $appRole['Project_role_id'] == $appRole['Project_role_id_u'])
+                    {
+                        $currentOrder = $appRole['Project_role_id'];
+
+                        array_push($roleStructure, array('Project_role_id' => $appRole['Project_role_id'], 'Role' => $appRole['Role']));
+
+                    }
+                    else if($appRole['Project_role_id_u'] == $currentOrder)
+                    {
+                        $currentOrder = $appRole['Project_role_id'];
+
+                        array_push($roleStructure, array('Project_role_id' => $appRole['Project_role_id'], 'Role' => $appRole['Role']));
+                    }
+
+                }
+            }
+
+            $roleLevel = null;
+
+            // If users role in role structure is called, get role and following roles in structure
+
+            $suitableRoles = array();
+
+            foreach($roleStructure as $roleItem) {
+
+                if($roleItem['Role'] == $roleInProject)
+                {
+                    $roleLevel = $roleItem['Role'];
+
+                    if($roleItem['Role'] != ucfirst(strtolower('General')))
+                    {
+                        array_push($suitableRoles, array('Project_role_id' => $roleItem['Project_role_id'], 'Role' => $roleItem['Role']));
+                    }
+                }
+                else if($roleLevel != null)
+                {
+                    array_push($suitableRoles, array('Project_role_id' => $roleItem['Project_role_id'], 'Role' => $roleItem['Role']));
+                }
+
+            }
+
+            if($suitableRoles != null)
+            {
+                return $suitableRoles;
+            }
+        }
+
+        return false;
+    }
+
+    /**
     * Function: CheckIfExist
     * This function is used in the formvalidation. Searches the
     * database for a match and returns the answer as an bool.
