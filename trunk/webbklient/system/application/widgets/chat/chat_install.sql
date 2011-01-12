b87aaba81b4fbe1d84f12f3e7cb1e1350a22e07d
astrom.tobias <astrom.tobias@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2011-01-12 14:11:59 +0000

    Updated with functions for fetch members...

---
+++ trunk/webbklient/system/application/widgets/chat/chat_install.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `WI_Chat_Room` (
+  `Chat_room_id` INT NOT NULL,
+  `Key` CHAR(32) NOT NULL,
+  `Project_id` INT NOT NULL,
+  PRIMARY KEY (`Chat_room_id`),
+  CONSTRAINT `chat_widget_ibfk_1` 
+    FOREIGN KEY (`Project_id`)
+    REFERENCES `Project` (`Project_id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
