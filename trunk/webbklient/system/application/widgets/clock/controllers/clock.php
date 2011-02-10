da2346f379de2e9afff30ab5b5f8e03f9392bfec
astrom.tobias <astrom.tobias@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2011-02-10 18:40:06 +0000

    Updated updateContentSize() function

---
+++ trunk/webbklient/system/application/widgets/clock/controllers/clock.php
@@ -0,0 +1,26 @@
+<?php
+
+class Clock extends Controller
+{
+
+    private $_widget_name = "clock";
+
+    function __construct()
+    {
+        parent::Controller();
+    }
+
+    function index()
+    {
+        $widget_name = $this->_widget_name;
+        $base_url = $this->config->item('base_url');
+        $data = array(
+            'base_url' => $base_url,
+            'widget_url' => site_url("/widget/$widget_name").'/',
+            'widget_base_url' => $base_url."system/application/widgets/$widget_name/"
+        );
+
+        $this->load->view_widget('start', $data);
+    }
+
+}
--- trunk/webbklient/system/application/widgets/clock/js/clock.js
+++ trunk/webbklient/system/application/widgets/clock/js/clock.js
@@ -1,104 +1,36 @@
 
-// place widget in a namespace (javascript object simulates a namespace)
-clockWidget = {
-	
-    pageContentDivClass: 'clock_main_content',
-    contentDivClass: 'clock_content',
-    widgetTitle: 'Clock',
-    widgetName: 'clock', // also name of folder
-    errorIcon: BASE_URL+'images/backgrounds/erroricon.png',
-    
-    // variable for window (DO NOT CHANGE - REQUIRED)
-    wnd: null, 
-    
-    // callbacks that is set in common.js upon start (DO NOT CHANGE - REQUIRED)     
-    onMinimize: null, 
-    onClose:null,
-    currentPartial: null,
-    // function that will be called upon start (REQUIRED - do NOT change the name)
-    open: function(project_widget_id, widgetIconId, last_position) {
-        			
-					var windowOptions = {
-		                 title: clockWidget.widgetTitle,
-	                     width: 150,
-	                     height: 130,
-	                     x: 10,
-	                     y: 15,
-	                     allowSettings: false,
-	                     resizable: false
-		             };
-		
-					Desktop.newWidgetWindow(project_widget_id, windowOptions, widgetIconId, clockWidget.partialContentDivClass, last_position);
-					
-					var loadFirstPage = SITE_URL+'/widget/' + clockWidget.widgetName + '/Pages/';
-				                        ajaxRequests.load(loadFirstPage, "clockWidget.setContent", "clockWidget.setAjaxError");
-		
-                } ,
-  
-				// set content in widgets div, called from the ajax request
-				    setContent: function(data) {
-				                        // The success return function, the data must be unescaped befor use.
-				                        // This is due to ILLEGAL chars in the string.	
-				                        Desktop.setWidgetContent(unescape(data));
-										
-                                        // find which .digiclock to use through selected window's children
-                                        $('#widget_' + Desktop.selectedWindowId ).find('.digiclock').jdigiclock({ }); 
-                                        
-                                        /*
-                                        OLD CODE; does not work with multiple instances
-                                        $('.digiclock').jdigiclock({ });
-                                        */
-				    },
+function clockWidget(id, wnd_options) {
 
-				    // set partial content in widgets div, called from the ajax request
-				    setPartialContent: function(data) {
-				                        // The success return function, the data must be unescaped befor use.
-				                        // This is due to ILLEGAL chars in the string.
-				                        Desktop.setWidgetPartialContent(this.currentPartial, unescape(data));
-				                        this.currentPartial = null;
-				    },
+    this.widgetName = "clock";
+    this.title = "Clock";
+    var partialClasses = [''];
 
-				    // set error-message in widgets div, called from the ajax request
-				    setAjaxError: function(loadURL) {
-				                        Desktop.show_ajax_error_in_widget(loadURL);
-				    },
+    // set options for window
+    wnd_options.title = this.title;
+    wnd_options.allowSettings = false;
+    wnd_options.resizable = false;
+    wnd_options.width = 150;
+    wnd_options.height = 130;
 
-				    // shows a message (example in start.php)
-				    example_showMessage: function(message) {
-				                        Desktop.show_message(message);    
-				                },
+    // Add settings event listener
+    Desktop.settingsEvent.addSettingsEventListener(id, "settingsEventTest");
 
-				    // wrapper-function that easily can be used inside views from serverside    
-				    loadURL: function(url) {
-				        // prepare url
-				        url = SITE_URL+'/widget/'+clockWidget.widgetName+url;
+    this.create(id, wnd_options, partialClasses);
 
-				        // send request
-				        ajaxRequests.load(url, 'clockWidget.setContent', 'clockWidget.setAjaxError');
-				    },
+}
 
-				                // Loads a ajaxrequest to specific partialclass, in this case "ajax_template_partial"
-				                loadURLtoPartialTest: function(url) {
-				        // prepare url
-				        url = SITE_URL+'/widget/'+clockWidget.widgetName+url;
+clockWidget.Inherits(Widget);
 
-				        // set currentpartial to to the classname
-				        this.currentPartial = clockWidget.partialContentDivClass;
+/*
+* Here comes all happening function. Executed from html links
+*
+*/
+// Overwriting the index function!
+clockWidget.prototype.index = function() {
 
-				        // send request, last parameter = true if this is a partial call. Will skip the loading image.
-				        ajaxRequests.load(url, 'clockWidget.setPartialContent', 'clockWidget.setAjaxError', true);
-				    },
+    // load the first page upon start
 
-				    // wrapper-function that easily can be used inside views from serverside
-				    postURL: function(formClass, url) {
-				        // prepare url
-				        url = SITE_URL+'/widget/'+clockWidget.widgetName+url;
+    var url = SITE_URL+'/widget/' + this.widgetName + '/clock/index/' + this.id;
+    ajaxRequests.load(this.id, url, "setWindowContent");
 
-				                                // catching the form data
-				                                var postdata = $('#widget_' + Desktop.selectedWindowId ).find('.' + formClass).serialize();
-
-				        // send request
-				        ajaxRequests.post(postdata, url, 'clockWidget.setContent', 'clockWidget.setAjaxError');   
-				    }
-
-				};
+}
