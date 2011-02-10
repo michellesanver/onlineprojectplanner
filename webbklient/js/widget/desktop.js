8857eda90822107f7d015e0c2e87bf9816f1a2aa
astrom.tobias <astrom.tobias@eb6c2c7c-1873-fdd5-8fb7-57b449c43e9e>
2011-02-10 18:38:39 +0000

    Updated updateContentSize() function

--- webbklient/js/widget/desktop.js
+++ webbklient/js/widget/desktop.js
@@ -538,7 +538,24 @@ Desktop = {
         var docHeight = $(document).height();
         var topBarHeight = $('#topbar').height();
         var wBarHeight = $('#widget_bar').height();
-        var contentHeight = (((docHeight - topBarHeight) - wBarHeight) - 20); // 20 is for margins
+        var contentHeight;
+
+        if($('#topbar').is(':visible') == false && $('#widget_bar').is(':visible') == false)
+        {
+            contentHeight = docHeight - 10; // 10 is for margins
+        }
+        else if($('#topbar').is(':visible') == false && $('#widget_bar').is(':visible') != false)
+        {
+            contentHeight = (docHeight - wBarHeight) - 10; // 10 is for margins
+        }
+        else if($('#widget_bar').is(':visible') == false)
+        {
+            contentHeight = (docHeight - topBarHeight) - 10; // 10 is for margins
+        }
+        else
+        {
+            contentHeight = ((docHeight - topBarHeight) - wBarHeight) - 10; // 10 is for margins
+        }
 
         $('#desktop').css('height',contentHeight+'px');
     },
